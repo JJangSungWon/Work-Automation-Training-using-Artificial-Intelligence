78c57cb1630c59de77a3d2fafe66b96f2705126f
jjangsungwon <dnjs2113@gmail.com>
2020-02-07 00:04:22 +0900

    변수의 type - int, float을 따로 지정해줄 필요 없이 대입되는 값을 보고 알아서 형이 정해진다.

---
+++ Step2_변수/type.py
@@ -0,0 +1,44 @@
+a = 10         # int
+b = 10.1       # float
+c = -1         # int
+d = True       # boolean
+e = 'good'     # str
+f = '10'       # str
+g = 'lee'      # str
+h = 'hojun'    # str 
+i = 'jun100!!' # str
+j = 10 + 2j    # complex
+k = 0b110      # int
+l = 0o56       # int 
+m = 0xAC       # int
+
+print('type(10): ', type(a))
+print('type(10.1): ', type(b))
+print('type(-1): ', type(c))
+print('type(True): ', type(d))
+print('type(\'good\'): ', type(e))
+print('\'10\' + \'10\': ', f+f)
+print('\'10\' * 3 ', f*3)
+print('\'lee\' + \'hojun\'', g + h)
+print('type(\'hojun\'): ', type(h))
+print('type(\'hojun100!!\'): ', type(i))
+print('type(10 + 2j) : ', type(j))
+print('type(0b110) : ', type(k))
+print(k)
+print('type(0o56) : ', type(l))
+print(l)
+print('type(0xAC) : ', type(m))
+print(m)
+      
+      
+
+
+
+
+
+
+
+
+
+
+
