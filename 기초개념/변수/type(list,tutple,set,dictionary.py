2f986b296cab435d80f5124c43ceeb5635d0c5f1
jjangsungwon <dnjs2113@gmail.com>
2020-02-07 12:48:01 +0900

    type(list, tuple, set, dictionary) - 기본 사용법, 형 확인

---
+++ 기초개념/변수/type(list,tutple,set,dictionary.py
@@ -0,0 +1,16 @@
+sampleList = [1, 1, 2, 2, 3, 3, 4, 4, 5, 7, 7, 7, 7, 7, 7]
+sampleTuple = (1, 2, 3, 4, 5, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7)
+sampleSet = {1, 2, 3, 4, 5, 7}
+sampleDictionary = {'one':1, 'two':2, 'three':3}
+
+print("List: ", sampleList, \ # \는 개행문자입니다.
+     ', type:', type(sampleList))
+
+print("Tuple: ", sampleTuple, \
+     ', type:', type(sampleTuple))
+
+print("Set: ", sampleSet, \
+     ', type:', type(sampleSet))
+
+print("Dictionary: ", sampleDictionary, \
+     ', type:', type(sampleDictionary))
