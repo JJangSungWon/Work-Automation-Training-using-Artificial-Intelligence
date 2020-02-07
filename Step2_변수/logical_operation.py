cfa975cd35fc11f1460e8b8537adc0042941a7e8
jjangsungwon <dnjs2113@gmail.com>
2020-02-07 12:24:15 +0900

    comment modification

--- Step2_변수/logical_operation.py
+++ Step2_변수/logical_operation.py
@@ -10,6 +10,6 @@ print(x and not y)           #True
 print(not x)                 #False
 
 # 아래는 True, False가 아니라 0, 1의 결과가 나왔다
-# boolean 형이 아닌 두 가지를 and, or 연산할때는 0 또는 1의 결과가 나오는 것 같다.
+# boolean 형이 아닌 두 가지를 and, or 연산할때는 bit 연산 결과가 나오는 것 같다.
 print(x and y) 
 print(y or x)
