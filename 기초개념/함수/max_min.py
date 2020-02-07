978689cad0a232077f2b21fd7a7413ec8ef23dbd
jjangsungwon <dnjs2113@gmail.com>
2020-02-07 19:57:41 +0900

    max, min 예시(list, tuple, set, dictionary)

---
+++ 기초개념/함수/max_min.py
@@ -0,0 +1,27 @@
+sampleList = [1,1,2,2,3,3,4,4,5,7,7,7,7,7,7]
+sampleTuple = (1,2,3,4,5,7,7,7,7,7,7,7,7,7,7)
+sampleSet = set(sampleList)
+sampleDictionary ={'one':1,'two':2,'three':3}
+
+maxList = max(sampleList)
+maxTuple = max(sampleTuple)
+maxSet = max(sampleSet)
+maxDictionary = max(sampleDictionary)
+
+minList = min(sampleList)
+minTuple = min(sampleTuple)
+minSet = min(sampleSet)
+minDictionary = min(sampleDictionary)
+
+print("max")
+print("maxList:", maxList)
+print("maxTuple:", maxTuple)
+print("maxSet:", maxSet)
+print("maxDictionary:", maxDictionary)
+
+print("\nmin")
+print("minList:", minList)
+print("minTuple:", minTuple)
+print("minSet:", minSet)
+print("minDictionary:", minDictionary)
+
